0049a737055cd870243277da3af57333daa832dc
Colin Strachan <col@strachanmusic.com>
2018-11-07 14:58:33 -0300

    simple form

---
+++ app/controllers/restaurants_controller.rb
@@ -0,0 +1,44 @@
+class RestaurantsController < ApplicationController
+  before_action :find_restaurant, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  def show
+  end
+
+  def new
+    @restaurant = Restaurant.new
+    @categories = ["chinese", "italian", "japanese", "french", "belgian"]
+  end
+
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    @restaurant.save
+    redirect_to restaurants_path
+  end
+
+  def edit
+  end
+
+  def update
+    @restaurant.update(restaurant_params)
+    redirect_to restaurants_path
+  end
+
+  def destroy
+    @restaurant.destroy
+    redirect_to restaurants_path
+  end
+
+  private
+
+  def find_restaurant
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :category, :address, :phone_number)
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+CATEGORIES = ["chinese", "italian", "japanese", "french", "belgian"]
+
+puts "Starting seeding process..."
+
+puts "Cleaning database!"
+
+Restaurant.destroy_all
+
+puts "Creating new Restaurants"
+
+10.times do
+  restaurant = {
+    name: Faker::Food.ingredient,
+    address: Faker::Address.street_address,
+    category: CATEGORIES.sample(1).first,
+    phone_number: Faker::Number.number(10)
+  }
+   Restaurant.create(restaurant)
+end
+
+puts "Finished seeding process!"
